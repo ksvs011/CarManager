54bc3bd762cc461726d5542870e6d30ee353639d
ksvs011 <ksvs011@gmail.com>
2020-07-13 15:28:11 +0900

    CarManager_xml

---
+++ CarManager_xml/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace CarManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnMaximizar.Visible = false;
+            btnRestaurar.Visible = true;
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnRestaurar.Visible = false;
+            btnMaximizar.Visible = true;
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")] // form 움직이게 하는 소스
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParamm, int lParam);
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MoveSidePanel(Control c)
+        {
+            SidePanel.Height = c.Height;
+            SidePanel.Top = c.Top;
+        }
+
+        private void btnMain_Click(object sender, EventArgs e)
+        {
+            MoveSidePanel(btnMain);
+            panelContent.Controls.Clear();
+            try
+            {
+                MainForm temp = new MainForm();
+
+                temp.TopLevel = false;
+                panelContent.Controls.Add(temp);
+                temp.Show();
+
+            }
+            catch (IndexOutOfRangeException ie)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void btnParkingInformation_Click(object sender, EventArgs e)
+        {
+            MoveSidePanel(btnParkingInformation);
+            panelContent.Controls.Clear();
+            try
+            {
+                ParkingForm temp = new ParkingForm();
+
+                temp.TopLevel = false;
+                panelContent.Controls.Add(temp);
+                temp.Show();
+
+            }
+            catch (IndexOutOfRangeException ie)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+   
+    }
+}
+
